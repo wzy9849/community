091bd6f1bf1a8bac08ef1ab532ce8015b8561aa7
wzy9849 <2530863582@qq.com>
2021-03-08 16:34:43 +0800

    init repo

---
+++ src/test/java/com/wzy/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzy.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
